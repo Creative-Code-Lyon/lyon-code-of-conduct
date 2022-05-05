4f9cc04b91d0532932707965a726c1cc376fdc8d
Yassin Siouda <yassin@siouda.com>
2022-05-05 23:36:48 +0100

    Added Sveltjs

---
+++ src/data/supporters.js
@@ -0,0 +1,14 @@
+const supporters = [
+    {
+        "name": "Le Sequenceur",
+        "location": "Lyon",
+        "website" : "https://www.lesequenceur.fr/"
+    },
+    {
+        "name": "Creative Code Lyon",
+        "location": "Lyon",
+        "website" : "https://creative-code-lyon.github.io/"
+    }
+]
+
+export default supporters
